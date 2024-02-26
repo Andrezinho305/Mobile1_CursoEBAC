48f5bc37ee3430a445449444673e0ca8259b1c4b
André Tavares <andre.t.teixeira@hotmail.com>
2024-02-26 16:16:49 -0300

    Módulo Mobile_Power Ups
    Created Flying power up - changes height and adds small upwards and dowards animation
    Created coin collecting power up - very bugged
    coins wont collect anymore
    rigidbody of coin collector is marked as player for some reason

---
+++ Assets/Scripts/Collectable/PowerUp/PowerUpBase.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBase : CollectableBase
+{
+    [Header("Power Up Duration")]
+    public float duration;
+
+
+    protected override void OnCollect() // override = sobreescreve a função orgiinal que está sendo herdada, adicionando comandos
+    { 
+        base.OnCollect(); 
+        StartPowerUp(); 
+    }
+
+    protected virtual void StartPowerUp() 
+    { 
+        Debug.Log("Start Power Up");
+        Invoke(nameof(EndPowerUp), duration); 
+    }
+
+    protected virtual void EndPowerUp() 
+    { 
+        Debug.Log("End Power Up"); 
+    }
+
+
+
+
+
+
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EBAC.Core.Singleton;
+using TMPro;
+using DG.Tweening;
 
-public class PlayerController : MonoBehaviour
+
+public class PlayerController : Singleton<PlayerController>
 {
     #region Variables
 
@@ -14,23 +18,43 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public float speed = 1f;
 
+    //public string wallTag = "Wall";
+
+
+    [Header("Text mesh")]
+    public TextMeshPro uiTextPowerUp;
 
     [Header("End Game Tags")]
     public string enemyTag = "Enemy";
     public string endTag = "EndLine";
+    public bool invincible = false;
 
+    [Header("End Game Stuff")]
     public GameObject endScreen;
 
+    [Header("Coin Setup")]
+    public GameObject coinCollector;
+
+
 
     #endregion
 
-    #region Private
+    #region Private Vars
     private Vector3 _pos;
     private bool _canRun;
+    private float _currentSpeed;
+    private Vector3 _startPosition;
 
     #endregion
 
     #endregion
+
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        ResetSpeed();
+    }
 
     void Update()
     {
@@ -39,17 +63,20 @@ public class PlayerController : MonoBehaviour
         _pos = target.position;
         _pos.y = transform.position.y;
         _pos.z = transform.position.z;
+        
+        //_pos.x = Mathf.Clamp(_pos.x, -5.5f, 5.5f);
 
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
+        
 
-        transform.Translate(transform.forward*speed*Time.deltaTime); //adiciona movimento para frente no transform do objeto, multiplicado pela velocidade definida, dentro do relógio padronizado (apara nao ser afetado por framerate)
+        transform.Translate(transform.forward*_currentSpeed*Time.deltaTime); //adiciona movimento para frente no transform do objeto, multiplicado pela velocidade definida, dentro do relógio padronizado (apara nao ser afetado por framerate)
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == enemyTag)
         {
-            EndGame();
+            if (!invincible) EndGame();
         }
     }
 
@@ -57,11 +84,9 @@ public class PlayerController : MonoBehaviour
     {
         if (other.transform.tag == endTag)
         {
-            EndGame();
+            if (!invincible) EndGame(); //se invincible = false, permite finalizar o jogo
         }
     }
-
-
 
     private void EndGame()
     {
@@ -73,4 +98,64 @@ public class PlayerController : MonoBehaviour
     {
         _canRun = true;
     }
+
+    public void SetPowerUpText(string s)
+    {
+        uiTextPowerUp.text = s;
+    }
+
+    #region Power Up: Speed
+
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f; //vira a nova velocidade
+    }
+
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+    #endregion
+
+    #region Power Up: Invincible
+
+    public void SetInvincible(bool b = true)
+    {
+        invincible = b;
+    }
+
+    #endregion
+
+    #region Power Up: Height
+
+    public void ChangeHeight(float amount, float duration, float animationDuration, Ease ease)
+    {
+        //Módo básico, sem animação
+        /*var p = transform.position; //salva a posição atual
+        p.y = _startPosition.y + amount; //altera a posição Y para a posição inicial + a definição de altura de voo definido 
+        transform.position = p;*/
+
+        transform.DOMoveY(_startPosition.y + amount,animationDuration).SetEase(ease);//.OnComplete(ResetHeight);a
+        Invoke(nameof(ResetHeight), duration);
+    }
+
+    public void ResetHeight(float animationDuration)
+    {
+        /*var p = transform.position;
+        p.y = _startPosition.y;
+        transform.position = p;*/
+
+        transform.DOMoveY(_startPosition.y, animationDuration);
+    }
+
+    #endregion
+
+    #region Power Up: Collect coins
+
+    public void ChangeCoinCollectorSize(float amount)
+    {
+        coinCollector.transform.localScale = Vector3.one * amount;
+    }
+
+    #endregion
 }
